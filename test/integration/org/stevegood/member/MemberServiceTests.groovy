8126f9878d5d06712c00935c98e42e6aac4465b0
Steve Good <steve@stevegood.org>
2011-02-24 12:55:43 -0600

    Added Member service integration test class

---
+++ test/integration/org/stevegood/member/MemberServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.stevegood.member
+
+import grails.test.*
+
+class MemberServiceTests extends GroovyTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
